e00c16c1863ad064c4f7092e7452b28d38a71a71
Florian Dietrich <florian.dietrich@ensem.org>
2020-01-24 21:11:02 +0100

    minimal cmake build for opinion dynamics with interactions

---
+++ tests/testOpinionWithInteractions.cpp
@@ -0,0 +1,11 @@
+#include "../src/socp/shooting.hpp"
+#include "../src/models/opinion_with_interactions/opinion_with_interactions.hpp"
+
+#include <iostream>
+
+int main ()
+{
+  std::cout << "SOCP for Opinion Dynamics With Interactions" << '\n';
+
+  return 0;
+}
